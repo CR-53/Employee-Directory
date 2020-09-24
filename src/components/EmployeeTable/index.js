2a665d0cc75fdef42caa29d3ee06c10026ba991b
Chris Roschi <chrisroschi53@gmail.com>
2020-09-24 13:18:09 +0930

    added filter and sorting to all columns

--- src/components/EmployeeTable/index.js
+++ src/components/EmployeeTable/index.js
@@ -3,6 +3,9 @@ import React, { useEffect, useState } from "react";
 import employeeData from "../../data/employees.json"
 import paginationFactory from "react-bootstrap-table2-paginator";
 import BootstrapTable from 'react-bootstrap-table-next';
+import filterFactory, { textFilter, numberFilter } from 'react-bootstrap-table2-filter';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSort } from '@fortawesome/free-solid-svg-icons'
 import * as ReactBootstrap from "react-bootstrap"
 
 const EmployeeTable = () => {
@@ -18,12 +21,30 @@ const EmployeeTable = () => {
     getEmployeeData();
   });
 
+  /* Table Header Names + Icon */
+  const sort = <FontAwesomeIcon icon={faSort} />
+  /* Employee ID */
+  const employeeIDText = `Employee ID`;
+  const employeeID = <React.Fragment>{employeeIDText} {sort}</React.Fragment>
+  /* First Name */
+  const firstNameText = `First Name`;
+  const firstName = <React.Fragment>{firstNameText} {sort}</React.Fragment>
+  /* Last Name */
+  const lastNameText = `Last Name`;
+  const lastName = <React.Fragment>{lastNameText} {sort}</React.Fragment>
+  /* Email */
+  const emailText = `Email`;
+  const email = <React.Fragment>{emailText} {sort}</React.Fragment>
+  /* Role */
+  const roleText = `Role`;
+  const role = <React.Fragment>{roleText} {sort}</React.Fragment>
+
   const columns = [
-    { dataField: "employee_id", text: "Employee ID" },
-    { dataField: "first_name", text: "First Name" },
-    { dataField: "last_name", text: "Last Name" },
-    { dataField: "email", text: "Email" },
-    { dataField: "role", text: "Role" }
+    { dataField: "employee_id", text: employeeID, sort: true, filter: numberFilter() },
+    { dataField: "first_name", text: firstName, sort: true, filter: textFilter() },
+    { dataField: "last_name", text: lastName, sort: true, filter: textFilter() },
+    { dataField: "email", text: email, sort: true, filter: textFilter() },
+    { dataField: "role", text: role, sort: true, filter: textFilter() }
   ]
 
   return (
@@ -32,8 +53,8 @@ const EmployeeTable = () => {
       data={employees}
       columns={columns}
       pagination={paginationFactory()}
+      filter={filterFactory()}
     />
-  
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 import 'react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css';
+import 'react-bootstrap-table2-filter/dist/react-bootstrap-table2-filter.min.css';
 
 
 
